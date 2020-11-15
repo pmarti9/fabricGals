b5c311d59950a1d9e0dc4b25943891757f06376a
pmarti99 <parkerjmartin883@gmail.com>
2020-11-14 23:06:33 -0700

    moving directories around, had one too many folders

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import react from "react"
+import "../Footer/Footer.css"
+
+function Footer(){
+    return(
+        <div>
+            <footer>
+                <br />
+                LeAnn Shaw, Lindsey Martin, Brooke Coleman
+                <br />
+                <br />
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
