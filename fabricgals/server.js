9728066dc1f80ce6c3e6c3548d0d68952a83ff6b
pmarti99 <parkerjmartin883@gmail.com>
2020-11-14 22:26:58 -0700

    modifying server.js to include the express app

--- fabricgals/server.js
+++ fabricgals/server.js
@@ -2,17 +2,12 @@ const express = require("express");
 // const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mongojs = require("mongojs")
-const mongoose = require("mongoose");
 //mongo databaseurl and collections
 // lines 8-13 .. is this in the server file or can i use the routes folder and reference it here.
-const databaseurl = "fabricgals"
-const collections = ["users","fabrics"];
-const db = mongojs(databaseurl, collections);
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
+
+const db = require("./models")
 // Routes.
+const app = express();
 app.use(express.static("routes"));
 
 // Define middleware here
@@ -22,10 +17,28 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(
+    session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
 
+
+    require("./routes/html-routes.js")(app);
+  require("./routes/api-routes.js")(app);
+  
+  app.use(express.static("public"));
 // Define API r//Adding comments
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ffdatabase", { useNewUrlParser: true });
 
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+      );
+    });
+  });
 
 // Send every other request to the React app
 // Define any API routes before this runs
